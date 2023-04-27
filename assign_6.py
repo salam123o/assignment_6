588a5b11f5a0bc04ad5154271c010632a0484289
salam123o <110718122+salam123o@users.noreply.github.com>
2023-04-27 20:39:00 +0530

    added python file

---
+++ assign_6.py
@@ -0,0 +1,32 @@
+friend_names=["santhosh","abhinay","ameer","nayeem","siraj","saleem","sadhik","shiva","gowtham","bhargav"]
+for p in friend_names:
+    print("my friend name is",p)
+
+
+family_names=["anwar","fathima","kalam","reshma","rowfa","areef","mobeen","mateen","amanu","ameen"]
+for f in family_names:
+    print(f)
+
+non_veg=["mutton biryani","chicken biryani","prawns biryani","fish biryani","chicken 65"]
+for a in non_veg:
+    print(a)
+
+veg=("veg biryani","veg fried rice","veg noodles","veg manchuria")
+for p in veg:
+    print(p)
+
+colour_names={"red","green","purple","yellow","white","black","blue","maroon","violet","sky blue"}
+for p in colour_names:
+    print(p)
+
+month_names=["january","february","march","april","may","june","july","august","september","october","november","december"]
+for p in month_names:
+    print(p)
+
+for p in range(0,90,9):
+    print(p)
+
+for p in range(0,120,12):
+    print(p)
+
+
